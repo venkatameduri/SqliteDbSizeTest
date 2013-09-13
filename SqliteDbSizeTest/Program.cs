27bd7184a70326878847eb28659b784a23341b3d
venkatameduri <github.meduri@gmail.com>
2013-09-12 18:41:37 -0700

    sqlite test project

---
+++ SqliteDbSizeTest/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SQLite;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace SqliteDbSizeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = Path.Combine(Directory.GetCurrentDirectory(), "user3contacts");
+            // Creates new sqlite database if it is not found
+            using (var conn = new SQLiteConnection(@"Data Source=" + filename))
+            {                
+                conn.Open();
+                var stopwatch = new Stopwatch();
+                stopwatch.Start();
+                using (var cmd = new SQLiteCommand(conn))
+                {                 
+                    for (int i = 1; i < 10000; i++)
+                    {
+                        cmd.CommandText = "INSERT INTO contactdata VALUES ('" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" +
+                            Guid.NewGuid().ToString() + "','" + 
+                            Guid.NewGuid().ToString() + "');";                        
+                        cmd.ExecuteNonQuery();
+                    }            
+                }
+
+                Console.WriteLine("{0} seconds with one transaction.", stopwatch.Elapsed.TotalSeconds);                
+            }
+        }
+
+    }
+}
